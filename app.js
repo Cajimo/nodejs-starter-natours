020004c4540c2b3cb1af5ae8856b3105b56768f7
Alex Mojica <alexmojicamtz@gmail.com>
2020-05-25 21:50:45 +0200

    Routes Handler

--- app.js
+++ app.js
@@ -17,11 +17,7 @@ app.use(express.json());
 // .static() The function determines the file to serve by combining req.url with the provided root directory.
 // The __dirname in a node script returns the path of the folder where the current JavaScript file resides.
 app.use(express.static(`${__dirname}/public`));
-// Just a demonstration.
-app.use((req, res, next) => {
-  console.log('Hola desde middleware');
-  next();
-});
+
 // Getting req date.
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -32,4 +28,14 @@ app.use((req, res, next) => {
 // Creating route paths.
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//Routes handler.
+// If we are able to reach this point here that it means that the  request response cycle
+// was not yet finished at this point in our code
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fallo',
+    mensaje: `No se encuentra ${req.originalUrl} en este servidor!`,
+  });
+});
 module.exports = app;
